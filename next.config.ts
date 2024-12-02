1916902606954ac5cc2c8ae0e8f401092551691a
Andcool-Systems <drolll2007@gmail.com>
2024-12-02 22:52:11 +0300

    fixed @svgr/webpack
    normal inline svg
    edit button showing animation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: false,
+    reactStrictMode: false,
+    experimental: {
+        turbo: {
+            rules: {
+                '*.svg': {
+                    loaders: ['@svgr/webpack'],
+                    as: '*.js',
+                },
+            },
+        },
+    },
+    webpack(config) {
+        config.cache = false;
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ["@svgr/webpack"],
+        });
+
+        return config;
+    },
 };
 
 export default nextConfig;
